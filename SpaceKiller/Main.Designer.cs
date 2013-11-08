e4cb9b9aa35c4f0909ad4ec3a8d9066d8c71848c
Vigo Zhong <xzoth2008@gmail.com>
2013-11-08 12:14:12 +0800

    upload

---
+++ SpaceKiller/Main.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace SpaceKiller
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.txtRegex = new System.Windows.Forms.TextBox();
+            this.txtSourceFile = new System.Windows.Forms.TextBox();
+            this.btnSelectSourceFile = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtReplaceString = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picLOGO = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLOGO)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReplace.Location = new System.Drawing.Point(419, 173);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(75, 23);
+            this.btnReplace.TabIndex = 0;
+            this.btnReplace.Text = "替换";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // txtRegex
+            // 
+            this.txtRegex.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRegex.Location = new System.Drawing.Point(90, 20);
+            this.txtRegex.Name = "txtRegex";
+            this.txtRegex.Size = new System.Drawing.Size(385, 21);
+            this.txtRegex.TabIndex = 1;
+            this.txtRegex.Text = "(?<=\".*)\\s+(?=\")";
+            // 
+            // txtSourceFile
+            // 
+            this.txtSourceFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSourceFile.Location = new System.Drawing.Point(6, 22);
+            this.txtSourceFile.Name = "txtSourceFile";
+            this.txtSourceFile.ReadOnly = true;
+            this.txtSourceFile.Size = new System.Drawing.Size(389, 21);
+            this.txtSourceFile.TabIndex = 2;
+            // 
+            // btnSelectSourceFile
+            // 
+            this.btnSelectSourceFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectSourceFile.Location = new System.Drawing.Point(401, 20);
+            this.btnSelectSourceFile.Name = "btnSelectSourceFile";
+            this.btnSelectSourceFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectSourceFile.TabIndex = 3;
+            this.btnSelectSourceFile.Text = "选择...";
+            this.btnSelectSourceFile.UseVisualStyleBackColor = true;
+            this.btnSelectSourceFile.Click += new System.EventHandler(this.btnSelectSourceFile_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.txtSourceFile);
+            this.groupBox1.Controls.Add(this.btnSelectSourceFile);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(482, 61);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "源文件";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.txtReplaceString);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtRegex);
+            this.groupBox2.Location = new System.Drawing.Point(13, 80);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(481, 87);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "设置";
+            // 
+            // txtReplaceString
+            // 
+            this.txtReplaceString.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtReplaceString.Location = new System.Drawing.Point(90, 52);
+            this.txtReplaceString.Name = "txtReplaceString";
+            this.txtReplaceString.Size = new System.Drawing.Size(385, 21);
+            this.txtReplaceString.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "替换字符";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "正则表达式";
+            // 
+            // picLOGO
+            // 
+            this.picLOGO.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.picLOGO.Image = ((System.Drawing.Image)(resources.GetObject("picLOGO.Image")));
+            this.picLOGO.Location = new System.Drawing.Point(12, 173);
+            this.picLOGO.Name = "picLOGO";
+            this.picLOGO.Size = new System.Drawing.Size(182, 23);
+            this.picLOGO.TabIndex = 7;
+            this.picLOGO.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(200, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 14);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "May May专用";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 207);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.picLOGO);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnReplace);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SpaceKiller";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLOGO)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.TextBox txtRegex;
+        private System.Windows.Forms.TextBox txtSourceFile;
+        private System.Windows.Forms.Button btnSelectSourceFile;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtReplaceString;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox picLOGO;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SpaceKiller/Main.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceKiller
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectSourceFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Title = "选择源文件";
+            fileDialog.Filter = "文本文件 |*.xml;*.txt;*.csv|" +
+                                "所有文件 |*.*";
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtSourceFile.Text = fileDialog.FileName;
+            }
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            string strSourceFile = txtSourceFile.Text;
+            string strRegText = txtRegex.Text;
+            string strReplaceText = txtReplaceString.Text;
+
+            if (string.IsNullOrWhiteSpace(strSourceFile))
+            {
+                MessageBox.Show("请先选择源文件", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            string strExtName = Path.GetExtension(strSourceFile);
+            string filter = string.Format("{0}文件| *{0}", string.IsNullOrEmpty(strExtName) ? "*" : strExtName);
+            saveFileDialog.Filter = filter;
+            saveFileDialog.OverwritePrompt = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string strSaveFilePath = saveFileDialog.FileName;
+                Encoding fileEncoding = TextFileUtil.GetFileEncodeType(strSourceFile);
+                string fileContent = File.ReadAllText(strSourceFile, fileEncoding);
+                string targetContent = Regex.Replace(fileContent, strRegText, strReplaceText);
+
+                File.WriteAllText(strSaveFilePath, targetContent, fileEncoding);
+            }
+        }
+    }
+}
